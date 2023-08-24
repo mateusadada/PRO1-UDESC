2b9ae896179445afcca13e0f52c7ae9d351f1d9e
Mateus Adada <125929822+mateusadada@users.noreply.github.com>
2023-08-24 16:56:23 -0300

    Update Programa_Principal.py
    Added the code

--- Exercícios resolvidos/VLP - Virtual Programming Lab/1 - Roupas/Programa_Principal.py
+++ Exercícios resolvidos/VLP - Virtual Programming Lab/1 - Roupas/Programa_Principal.py
@@ -3,8 +3,14 @@ from Roupa import Roupa
 
 ##################################################
 
-## Questão 07: Qual o código necessário para criar o programa principal?
+# Questão 07: Qual o código necessário para criar o programa principal?
+teste=Roupa()
+teste.Leitura()
+teste.Calcula_Total()
+print(teste.toString())
+del(teste)
 
-## Questão 08: Qual o comando que encerra o programa principal?
+# Questão 08: Qual o comando que encerra o programa principal?
+sys.exit(0)
 
 ##################################################
