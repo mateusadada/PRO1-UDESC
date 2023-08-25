59f143940a675cada4c652744fe17c00d57856d7
Mateus Adada <125929822+mateusadada@users.noreply.github.com>
2023-08-25 10:35:09 -0300

    Update Programa_Principal.py
    Added the code

--- Exercícios resolvidos/VLP - Virtual Programming Lab/2 - Camisas/Programa_Principal.py
+++ Exercícios resolvidos/VLP - Virtual Programming Lab/2 - Camisas/Programa_Principal.py
@@ -3,8 +3,14 @@ from Camisa import Camisa
 
 ##################################################
 
-## Questão 13: Qual o código necessário para criar o programa principal?
+# Questão 13: Qual o código necessário para criar o programa principal?
+adidas=Camisa()
+adidas.Leitura()
+adidas.Calcula_Total()
+print(adidas.toString())
+del(adidas)
 
-## Questão 14: Qual o comando que encerra o programa principal?
+# Questão 14: Qual o comando que encerra o programa principal?
+sys.exit(0)
 
 ##################################################
