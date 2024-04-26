5c9dc44923fef24e4e2bb6889be9a6b29cfba02d
Mateus Adada <125929822+mateusadada@users.noreply.github.com>
2024-04-26 16:17:34 -0300

    Update Programa_Principal.py
    Removed '#'

--- solved_exercises/VPL/3 - IPTU/Programa_Principal.py
+++ solved_exercises/VPL/3 - IPTU/Programa_Principal.py
@@ -2,8 +2,6 @@ import sys
 
 from Contribuinte import Contribuinte
 
-##################################################
-
 # Questão 13: Qual o código necessário para criar o programa principal?
 pessoa=Contribuinte()
 pessoa.Leitura()
@@ -13,5 +11,3 @@ del(pessoa)
 
 # Questão 14: Qual o comando que encerra o programa principal?
 sys.exit(0)
-
-##################################################
