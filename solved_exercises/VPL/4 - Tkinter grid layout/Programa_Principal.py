981fd9851b08d96b77c1b8afb7172d820d109a97
Mateus Adada <125929822+mateusadada@users.noreply.github.com>
2024-05-06 13:59:32 -0300

    Update Programa_Principal.py
    Removed '#'

--- solved_exercises/VPL/4 - Tkinter grid layout/Programa_Principal.py
+++ solved_exercises/VPL/4 - Tkinter grid layout/Programa_Principal.py
@@ -1,10 +1,4 @@
 from Janela import Janela
 
-##################################################
-
-# janela=Janela("Minha janela Tkinter")
 janela = Janela("Minha janela Tkinter")
-# janela.mainloop()
 janela.mainloop()
-
-##################################################
