1243c028943ac2b5dee8abc5ddd13ed2812c1387
Mateus Adada <125929822+mateusadada@users.noreply.github.com>
2024-05-09 16:12:56 -0300

    Update Programa_Principal.py
    Removed '#'

--- solved_exercises/VPL/6 - Tkinter torcidas/Programa_Principal.py
+++ solved_exercises/VPL/6 - Tkinter torcidas/Programa_Principal.py
@@ -1,9 +1,5 @@
 from Janela import Janela
 
-##################################################
-
 # Questão 10: (Aloque um objeto da classe Janela e mostre-o na tela)
 janela=Janela("Torcidas de Futebol")
 janela.mainloop()
-
-##################################################
