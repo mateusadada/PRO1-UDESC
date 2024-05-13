7468f356b95f4b6e67d864e8eb2953665daacc43
Mateus Adada <125929822+mateusadada@users.noreply.github.com>
2024-05-13 13:00:39 -0300

    Update Programa_Principal.py
    Removed '#'

--- solved_exercises/VPL/2 - Camisas/Programa_Principal.py
+++ solved_exercises/VPL/2 - Camisas/Programa_Principal.py
@@ -1,8 +1,6 @@
 import sys
 from Camisa import Camisa
 
-##################################################
-
 # Questão 13: Qual o código necessário para criar o programa principal?
 adidas=Camisa()
 adidas.Leitura()
@@ -12,5 +10,3 @@ del(adidas)
 
 # Questão 14: Qual o comando que encerra o programa principal?
 sys.exit(0)
-
-##################################################
