7737363ada7b35dbc15dde36a992f862c9ac7b87
Mateus Adada <125929822+mateusadada@users.noreply.github.com>
2024-05-16 13:07:00 -0300

    Update Programa_Principal.py
    Removed "#"

--- solved_exercises/VPL/9 - Atividade física/Programa_Principal.py
+++ solved_exercises/VPL/9 - Atividade física/Programa_Principal.py
@@ -1,9 +1,5 @@
 from Janela import Janela
 
-##################################################
-
 # Questão 11: (Aloque um objeto da classe Janela e mostre-o na tela)
 janela=Janela("Atividade Física")
 janela.mainloop()
-
-##################################################
